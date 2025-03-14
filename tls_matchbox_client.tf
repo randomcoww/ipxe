a8d28e9b47f6344cefedbd5b03516e01557fa3c3
randomcoww <averyfuzzybunny@gmail.com>
2025-03-14 15:28:51 -0700

    add privkey again

--- tls_matchbox_client.tf
+++ tls_matchbox_client.tf
@@ -23,7 +23,7 @@ resource "tls_cert_request" "matchbox-client" {
   private_key_pem = tls_private_key.matchbox-client.private_key_pem
 
   subject {
-    common_name = "matchbox"
+    common_name = "matchbox-client"
   }
 }
 
